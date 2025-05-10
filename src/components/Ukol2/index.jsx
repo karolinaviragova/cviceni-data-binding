21734fc34ca64756efe61bca28e271d2c5861cdc
Karolina Viragova <karolinavi94@gmail.com>
2025-05-10 10:33:10 +0200

    solution

--- src/components/Ukol2/index.jsx
+++ src/components/Ukol2/index.jsx
@@ -8,12 +8,12 @@ Zadání: Přidejte na prvek `input` událost `onChange`, která bude do stavu `
 
 export const Ukol2 = () => {
   const [login, setLogin] = useState('petr');
-
+  //console.log(login)
   return (
     <>
       <label>
         Login:
-        <input type="text" />
+        <input type="text" value={login} onChange={(e) => {setLogin(e.target.value)}}/>
       </label>
     </>
   );
